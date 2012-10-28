0b65e1497f8a92ab19244f37ded311b678f40ecb
Alex Lourie <alourie@redhat.com>
2012-10-28 18:43:07 +0200

    updating the code
    * Adding correct properties to Project/Issue objects
    * Updating to work with json intead of xml

--- redmine.py
+++ redmine.py
@@ -6,56 +6,21 @@ import xml.etree.ElementTree as ET
 class _Project:
     '''Object returned by Redmine getProject calls
        redmine is the redmine object.
-       objXML is the xml object containing the object data'''
+       objPr is the json object containing the project data'''
 
-    def __init__(self, redmine, eTree=None ):
+    def __init__(self, redmine):
         self.__redmine = redmine
-        self.root = None
-        self.number = None
+        self.description = None
+        self.homepage = None
         self.id = None
         self.name = None
-        self.custom = {}
-        self.tracker = {}
+        self.createdon = None
+        self.updateon = None
 
-        if eTree:
-            try:
-                self.parseETree( eTree )
-            except:
-                if self.__redmine.readonlytest:
-                    self.THIS_IS_FAKE_DATA = True
-                    self.number = '6789'
-                    self.name = 'Fake Project'
-                    self.id = 'fakeproject'
-                    return
-                else:
-                    raise
-
-    def parseETree(self, eTree ):
-        self.root = eTree.getroot()
-
-        if self.root.tag == 'projects':
-            raise TypeError ('XML does not describe a single project, but a collection of projects.')
-        elif not self.root.tag == 'project':
-            raise TypeError ('XML does not describe a project as I know it.')
-
-        self.number = self.root.find('id').text
-        self.id = self.root.find('identifier').text
-        self.name = self.root.find('name').text
-
-#        custom_fields = self.root.find('custom_fields')
-#        if len(custom_fields) > 0:
-#            for field in custom_fields:
-#                if field.attrib and "name" in field.attrib.keys():
-#                    self.custom[ field.attrib['name'] ] = field.text
-
-#        for tracker in self.root.find('trackers'):
-#            self.tracker[ tracker.attrib['name'] ] = tracker.attrib['id']
-
-
-    def newIssue(self, **data ):
+    def newIssue(self, **data):
         '''Create a new issue for this project from the given pairs.
 
-        newIssue( subject="The Arguments Department is closed", description="Good morning." )
+        newIssue( subject="The Arguments Department is closed", description="Good morning.")
 
         Possible keys are:
          subject
@@ -70,20 +35,20 @@ class _Project:
         print "Got params: %s" % data
 
         if not 'subject' in data.keys():
-            raise TypeError('Subject cannot be blank.  Use subject=str')
+            raise TypeError('Subject field cannot be blank.  Use newIssue(subject=str) to create a new issue.')
 
-        data[ 'project_id' ] = self.number
-        return self.__redmine.newIssueFromDict( data )
+        # TODO: what is the project_id ?? Should be initialized, of course
+        data['project_id'] = self.id
+        return self.__redmine.newIssueFromDict(data)
 
-    def getIssues(self ):
+    def getIssues(self):
         pass
         #todo: finish
 
 
-
 class _Issue:
     '''Object returned by Redmine getIssue and newIssue calls'''
-    def __init__(self, redmine, eTree=None ):
+    def __init__(self, redmine, data):
         self.__redmine = redmine
 
         self.id = None
@@ -95,56 +60,41 @@ class _Issue:
         self.status = None
         self.description = None
 
-        if eTree:
+        if data:
             try:
-                self.parseETree( eTree )
+                self.parseData(data)
             except:
-                if self.__redmine.readonlytest:
-                    self.THIS_IS_FAKE_DATA = True
-                    self.id = '12345'
-                    self.subject = 'Fake Issue'
-                    self.project = '6789'
-                    self.status = '1'
-                    return
-                else:
-                    raise
+                raise
 
+    def parseData(self, data):
+        '''Parse fields from given Issue object'''
 
-    def parseETree(self, eTree):
-        '''Parse fields from given eTree into this object'''
-        self.root = eTree.getroot()
-
-        if self.root.tag == 'issues':
-            raise TypeError ('XML does not describe a single issue, but a collection of issues.')
-        elif not self.root.tag == 'issue':
-            raise TypeError ('XML does not describe an issue as I know it.')
-
-        self.id         = self.root.find('id').text
-        self.subject    = self.root.find('subject').text
-        self.project    = self.root.find('project').attrib
-        self.tracker    = self.root.find('tracker').attrib
-        self.status     = self.root.find('status').attrib
+        # TODO: actually parse the Issue data object
+        self.id        = self.root.find('id').text
+        self.project   = self.root.find('project').attrib
+        self.tracker   = self.root.find('tracker').attrib
+        self.status    = self.root.find('status').attrib
+        self.priority  = self.root.find('status').attrib
+        self.author    = self.root.find('status').attrib
+        self.assigned_to = self.root.find('status').attrib
+        self.subject   = self.root.find('subject').text
         self.description = self.root.find('description').text
+        self.start_date = self.root.find('description').text
+        self.due_date = self.root.find('description').text
+        self.done_ratio = self.root.find('description').text
+        self.estimated_hours = self.root.find('description').text
+        self.spent_hours = self.root.find('description').text
+        self.created_on = self.root.find('description').text
+        self.update_on = self.root.find('description').text
 
-        try:
-            self.assigned_to = self.root.find('assigned_to').attrib
-        except:
-            pass
-
-
-#        for field in self.root.find('custom_fields'):
-#            self.custom[ field.attrib['name'] ] = field.text
-
-#        for field in self.root.find('relations'):
-#            self.relations[ field.attrib['issue_id'] ] = field.attrib
 
     def resolve(self):
         '''Resolve this issue'''
-        self.__redmine.resolveIssue( self.id )
+        self.__redmine.resolveIssue(self.id)
 
     def close(self):
         '''Close this issue'''
-        self.__redmine.closeIssue( self.id )
+        self.__redmine.closeIssue(self.id)
 
     def save(self):
         '''Saves this issue - updates or creates new issue as necessary.  Failed updates DO NOT return any errors.'''
@@ -153,9 +103,9 @@ class _Issue:
 
 class Redmine:
     '''Class to interoperate with a Redmine installation using the REST web services.
-    instance = Redmine(url, [key=strKey], [username=strName, password=strPass] )
+    instance = Redmine(url, [key=strKey], [username=strName, password=strPass])
 
-    url is the base url of the Redmine install ( http://my.server/redmine )
+    url is the base url of the Redmine install ( http://my.server/redmine)
 
     key is the user API key found on the My Account page for the logged in user
         All interactions will take place as if that user were performing them, and only
@@ -165,7 +115,7 @@ class Redmine:
     If neither are defined, then only publicly visible items will be retreived
     '''
 
-    def __init__(self, url, key=None, username=None, password=None, debug=False, readonlytest=False ):
+    def __init__(self, url, key=None, username=None, password=None, debug=False, readonlytest=False):
         self.__url = url
         self.__key = key
         self.debug = debug
@@ -192,38 +142,37 @@ class Redmine:
             self.__key = None
 
         if not password:
-            password = '12345'  #the same combination on my luggage!  (dummy value)
+            password = '12345'  # the same combination on my luggage!  (dummy value)
 
-        if( username and password ):
+        if(username and password):
             #realm = 'Redmine API'
             # create a password manager
             password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
 
-            password_mgr.add_password(None, url, username, password )
-            handler = urllib2.HTTPBasicAuthHandler( password_mgr )
+            password_mgr.add_password(None, url, username, password)
+            handler = urllib2.HTTPBasicAuthHandler(password_mgr)
 
             # create "opener" (OpenerDirector instance)
-            self.__opener = urllib2.build_opener( handler )
+            self.__opener = urllib2.build_opener(handler)
 
             # set the opener when we fetch the URL
-            self.__opener.open( url )
+            self.__opener.open(url)
 
             # Install the opener.
-            urllib2.install_opener( self.__opener )
+            urllib2.install_opener(self.__opener)
 
         else:
             if not key:
                 pass
                 #raise TypeError('Must pass a key or username and password')
 
-
-    def Issue(self, eTree=None ):
+    def Issue(self, data):
         '''Issue object factory'''
-        return _Issue( self, eTree )
+        return _Issue(self, data)
 
-    def Project(self, eTree=None ):
-        '''Issue project factory'''
-        return _Project( self, eTree )
+    def Project(self, data):
+        '''Project obect factory'''
+        return _Project(self, data)
 
     # extend the request to handle PUT command
     class PUT_Request(urllib2.Request):
@@ -235,12 +184,10 @@ class Redmine:
         def get_method(self):
             return 'DELETE'
 
-
-
-    def openRaw(self, page, parms=None, XMLstr=None, HTTPrequest=None, dict=None ):
+    def openRaw(self, page, parms=None, XMLstr=None, HTTPrequest=None, dict=None):
         '''Opens a page from the server with optional XML.  Returns a response file-like object'''
         if not parms:
-            parms={}
+            parms = {}
 
         # if we're using a key, add it to the parms array
         if self.__key:
@@ -249,14 +196,13 @@ class Redmine:
         # encode any data
         urldata = ''
         if parms:
-            urldata = '?' + urllib.urlencode( parms )
-
+            urldata = '?' + urllib.urlencode(parms)
 
         fullUrl = self.__url + '/' + page
 
         # register this url to be used with the opener
         if self.__opener:
-            self.__opener.open( fullUrl )
+            self.__opener.open(fullUrl)
 
         #debug
         if self.debug:
@@ -264,99 +210,97 @@ class Redmine:
 
         # Set up the request
         if HTTPrequest:
-            request = HTTPrequest( fullUrl + urldata )
+            request = HTTPrequest(fullUrl + urldata)
         else:
-            request = urllib2.Request( fullUrl + urldata )
+            request = urllib2.Request(fullUrl + urldata)
         # get the data and return XML object or Json
         if XMLstr:
             request.add_header('Content-Type', 'application/xml')
-            response = urllib2.urlopen( request, XMLstr )
+            response = urllib2.urlopen(request, XMLstr)
         elif dict:
             request.add_header('Content-Type', 'application/json')
-            response = urllib2.urlopen( request, urllib.urlencode(dict) )
+            response = urllib2.urlopen(request, urllib.urlencode(dict))
         else:
-            response = urllib2.urlopen( request )
+            response = urllib2.urlopen(request)
 
         if self.debug:
             print "Response is:\n %s\n" % response
         return response
 
-    def open(self, page, parms=None, objXML=None, HTTPrequest=None, dict=None ):
+    def open(self, page, parms=None, objXML=None, HTTPrequest=None, dict=None):
         '''Opens a page from the server with optional XML.  Returns an XML ETree object or string if return value isn't XML'''
-        response = self.openRaw( page, parms, objXML, HTTPrequest, dict )
+        response = self.openRaw(page, parms, objXML, HTTPrequest, dict)
         try:
             etree = ET.ElementTree()
-            etree.parse( response )
+            etree.parse(response)
             return etree
         except:
             return response.read()
 
+    def get(self, page, parms=None):
+        '''Gets a JSON object from the server - used to read Redmine items.'''
+        return self.open(page, parms)
 
-    def get(self, page, parms=None ):
-        '''Gets an XML object from the server - used to read Redmine items.'''
-        return self.open( page, parms )
-
-    def post(self, page, objXML, parms=None ):
-        '''Posts an XML object to the server - used to make new Redmine items.  Returns an XML object.'''
+    def post(self, page, objJSON, parms=None):
+        '''Posts a JSON object to the server - used to make new Redmine items.  Returns an XML object.'''
         if self.readonlytest:
             print 'Redmine read only test: Pretending to create: ' + page
-            return objXML
+            return objJSON
         else:
-            return self.open( page=page, parms=parms, dict=objXML )
+            return self.open(page=page, parms=parms, dict=objJSON)
 
-    def put(self, page, objXML, parms=None ):
-        '''Puts an XML object on the server - used to update Redmine items.  Returns nothing useful.'''
+    def put(self, page, objJSON, parms=None):
+        '''Puts a JSON object on the server - used to update Redmine items.  Returns nothing useful.'''
         if self.readonlytest:
             print 'Redmine read only test: Pretending to update: ' + page
         else:
-            return self.open( page, parms, objXML, HTTPrequest=self.PUT_Request )
+            return self.open(page, parms, objJSON, HTTPrequest=self.PUT_Request)
 
-    def delete(self, page ):
+    def delete(self, page):
         '''Deletes a given object on the server - used to remove items from Redmine.  Use carefully!'''
         if self.readonlytest:
             print 'Redmine read only test: Pretending to delete: ' + page
         else:
-            return self.open( page, HTTPrequest=self.DELETE_Request )
+            return self.open(page, HTTPrequest=self.DELETE_Request)
 
-    def dict2XML(self, tag, dict ):
+    def dict2XML(self, tag, dict):
         '''Return an XML string encoding the given dict'''
-        root = ET.Element( tag )
+        root = ET.Element(tag)
         for key in dict:
-            ET.SubElement( root, str(key) ).text = str(dict[key])
+            ET.SubElement(root, str(key)).text = str(dict[key])
 
-        return ET.tostring( root, encoding='UTF-8' )
+        return ET.tostring(root, encoding='UTF-8')
 
+    def getProject(self, projectIdent):
+        '''returns a JSON project object for the given project name'''
+        return self.Project(self.get('projects/' + projectIdent + '.json'))
 
-    def getProject(self, projectIdent ):
-        '''returns a project object for the given project name'''
-        return self.Project( self.get('projects/'+projectIdent+'.xml') )
+    def getIssue(self, issueID):
+        '''returns a JSON issue object for the given issue number'''
+        return self.Issue(self.get('issues/' + str(issueID) + '.json'))
 
-    def getIssue(self, issueID ):
-        '''returns an issue object for the given issue number'''
-        return self.Issue( self.get('issues/'+str(issueID)+'.xml') )
-
-    def newIssueFromDict(self, dict ):
+    def newIssueFromDict(self, dict):
         '''creates a new issue using fields from the passed dictionary.  Returns the issue number or None if it failed. '''
-        #xmlStr = self.dict2XML( 'issue', dict )
+        #xmlStr = self.dict2XML( 'issue', dict)
         #print "xmlStr is \n%s\n" % xmlStr
-        newIssue = self.Issue( self.post( 'issues.xml', dict ) )
+        newIssue = self.Issue(self.post('issues.json', dict))
         return newIssue
 
-    def updateIssueFromDict(self, ID, dict ):
+    def updateIssueFromDict(self, ID, dict):
         '''updates an issue with the given ID using fields from the passed dictionary'''
-        xmlStr = self.dict2XML( 'issue', dict )
-        self.put( 'issues/'+str(ID)+'.xml', xmlStr )
+#        xmlStr = self.dict2XML('issue', dict)
+        self.put('issues/' + str(ID) + '.json', dict)
 
-    def deleteIssue(self, ID ):
+    def deleteIssue(self, ID):
         '''delete an issue with the given ID.  This can't be undone - use carefully!
         Note that the proper method of finishing an issue is to update it to a closed state.'''
-        self.delete( 'issues/'+str(ID)+'.xml' )
+        self.delete('issues/' + str(ID) + '.xml')
 
-    def closeIssue(self, ID ):
+    def closeIssue(self, ID):
         '''close an issue by setting the status to self.ISSUE_STATUS_ID_CLOSED'''
-        self.updateIssueFromDict( ID, {'status_id':self.ISSUE_STATUS_ID_CLOSED} )
+        self.updateIssueFromDict(ID, {'status_id': self.ISSUE_STATUS_ID_CLOSED})
 
-    def resolveIssue(self, ID ):
+    def resolveIssue(self, ID):
         '''close an issue by setting the status to self.ISSUE_STATUS_ID_RESOLVED'''
-        self.updateIssueFromDict( ID, {'status_id':self.ISSUE_STATUS_ID_RESOLVED} )
+        self.updateIssueFromDict(ID, {'status_id': self.ISSUE_STATUS_ID_RESOLVED})
 
